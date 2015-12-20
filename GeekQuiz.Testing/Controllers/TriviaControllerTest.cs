e87ed5778b00b177e448765653f21029db10f9f4
David Goldstein <dgavian@hotmail.com>
2015-12-20 09:31:26 -0700

    Add Geek Quiz code

---
+++ GeekQuiz.Testing/Controllers/TriviaControllerTest.cs
@@ -0,0 +1,129 @@
+﻿using GeekQuiz.Controllers;
+using GeekQuiz.Models;
+using GeekQuiz.WorkerServices;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace GeekQuiz.Testing.Controllers
+{
+  [TestFixture]
+  [Category("NUnit")]
+  public class TriviaControllerTest
+  {
+    private const string _USER = "Test";
+
+    [Test]
+    public void TriviaController_Ctor_ThrowsWithNullService()
+    {
+      ITriviaService service = null;
+      var user = MakeUser();
+
+      var ex = Assert.Catch<ArgumentNullException>(() => MakeSut(service, user));
+
+      StringAssert.Contains("service", ex.Message);
+    }
+
+    [Test]
+    public void CurrentUser_SetterWithNullValue_Throws()
+    {
+      var service = new Mock<ITriviaService>().Object;
+      IPrincipal user = null;
+
+      var ex = Assert.Catch<ArgumentNullException>(() => MakeSut(service, user));
+
+      StringAssert.Contains("value", ex.Message);
+    }
+
+    [Test]
+    public void CurrentUser_SetterSetMoreThanOnce_Throws()
+    {
+      var service = new Mock<ITriviaService>().Object;
+      var sut = MakeSut(service);
+      // Getting lazy loaded value invokes the setter.
+      var currentUser = sut.CurrentUser;
+
+      var ex = Assert.Catch<InvalidOperationException>(() => sut.CurrentUser = MakeUser());
+    }
+
+    [Test]
+    public void CurrentUser_SetOnce_ReturnsExpectedUserName()
+    {
+      var userName = "TestSetCurrentUser";
+      var service = new Mock<ITriviaService>().Object;
+      var sut = MakeSut(service);
+
+      sut.CurrentUser = MakeUser(userName);
+
+      Assert.AreEqual(userName, sut.CurrentUser.Identity.Name);
+    }
+
+    [Test]
+    public async Task Get_ServiceReturnsValidQuestion_Returns200()
+    {
+      var service = new Mock<ITriviaService>();
+      var response = new TriviaQuestion();
+      service.Setup(s => s.NextQuestionAsync(It.IsAny<string>())).ReturnsAsync(response);
+      var user = MakeUser();
+      var sut = MakeSut(service.Object, user);
+
+      var actual = await sut.Get() as OkNegotiatedContentResult<TriviaQuestion>;
+
+      Assert.IsNotNull(actual);
+    }
+
+    [Test]
+    public async Task Get_ServiceReturnsNullQuestion_Returns404()
+    {
+      var service = new Mock<ITriviaService>();
+      TriviaQuestion response = null;
+      service.Setup(s => s.NextQuestionAsync(It.IsAny<string>())).ReturnsAsync(response);
+      var user = MakeUser();
+      var sut = MakeSut(service.Object, user);
+
+      var actual = await sut.Get() as NotFoundResult;
+
+      Assert.IsNotNull(actual);
+    }
+
+    [Test]
+    public async Task Post_ServiceWithMockData_ReturnsExpectedValue()
+    {
+      var service = new Mock<ITriviaService>();
+      service.Setup(s => s.StoreAsync(It.IsAny<TriviaAnswer>())).ReturnsAsync(true);
+      var user = MakeUser();
+      var sut = MakeSut(service.Object, user);
+      var answer = new TriviaAnswer();
+
+      var actual = await sut.Post(answer) as OkNegotiatedContentResult<bool>;
+
+      Assert.IsNotNull(actual);
+      Assert.AreEqual(true, actual.Content);
+    }
+
+    private TriviaController MakeSut(ITriviaService service)
+    {
+      var result = new TriviaController(service);
+      return result;
+    }
+
+    private TriviaController MakeSut(ITriviaService service, IPrincipal user)
+    {
+      var result = new TriviaController(service);
+      result.CurrentUser = user;
+      return result;
+    }
+
+    private IPrincipal MakeUser(string userName = _USER)
+    {
+      var mockIdentity = new Mock<IIdentity>();
+      mockIdentity.Setup(i => i.Name).Returns(userName);
+      var mockPrincipal = new Mock<IPrincipal>();
+      mockPrincipal.Setup(p => p.Identity).Returns(mockIdentity.Object);
+      return mockPrincipal.Object;
+    }
+  }
+}
---
+++ GeekQuiz.Testing/Scripts/app/TriviaServiceSpec.js
@@ -0,0 +1,71 @@
+﻿/// <reference path="../jasmine-html.js" />
+/// <reference path="../jasmine.js" />
+/// <reference path="../../../geekquiz/scripts/angular.js" />
+/// <reference path="../../../geekquiz/scripts/angular-mocks.js" />
+/// <reference path="../../../geekquiz/scripts/app/app.js" />
+/// <reference path="../../../geekquiz/scripts/app/quiz-controller.js" />
+/// <reference path="../../../geekquiz/scripts/app/trivia-service.js" />
+
+(function () {
+    'use strict';
+
+    describe('Service: triviaService', function () {
+        var sut, backend,
+            options = [{
+                id: 1,
+                title: '1',
+                questionId: 1
+            }, {
+                id: 2,
+                title: '2',
+                questionId: 1
+            }, {
+                id: 3,
+                title: '4',
+                questionId: 1
+            }, {
+                id: 4,
+                title: '8',
+                questionId: 1
+            }],
+            question = 'What is two times four?',
+            data = { options: options, title: question };
+
+        beforeEach(module('QuizApp'));
+
+        beforeEach(function () {
+            inject(function ($httpBackend, _triviaService_) {
+                sut = _triviaService_;
+                backend = $httpBackend;
+            });
+        });
+
+        afterEach(function () {
+            backend.verifyNoOutstandingExpectation();
+            backend.verifyNoOutstandingRequest();
+        });
+
+        it('should get expected data', function () {
+            var returnedPromise, result;
+            backend.expectGET('/api/trivia').respond(data);
+            returnedPromise = sut.nextQuestion();
+            returnedPromise.then(function (response) {
+                result = response;
+            });
+            backend.flush();
+            expect(result.options.length).toEqual(4);
+            expect(result.title).toEqual('What is two times four?');
+        });
+
+        it('should post and return expected response', function () {
+            var returnedPromise, result, option = {};
+            backend.expectPOST('/api/trivia').respond(true);
+            returnedPromise = sut.sendAnswer(option);
+            returnedPromise.then(function (response) {
+                result = response;
+            });
+            backend.flush();
+            expect(result).toEqual(true);
+        });
+    });
+}());
